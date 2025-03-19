fad01edbbb9d193be46c2b6462d15f041ca0286c
mohammedali <mohaalialalawi20@gmail.com>
2025-03-19 11:32:57 +0400

    ADD REMOVE DEPLUCATE 2

--- Program.cs
+++ Program.cs
@@ -1143,7 +1143,31 @@ namespace tasks
 
 
 
+        static void RemoveDuplicates()
+        {
+            Console.Write("Enter the number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
 
+            Console.WriteLine("Enter elements:");
+            for (int i = 0; i < n; i++) arr[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Array without duplicates:");
+            for (int i = 0; i < n; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
 
     }
 }
